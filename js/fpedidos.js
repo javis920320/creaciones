3b8655268138428023ce544cd295ed09197a66e6
javier <javis9203@gmail.com>
2017-08-10 17:58:32 -0500

    ppp
    pp

--- js/fpedidos.js
+++ js/fpedidos.js
@@ -16,18 +16,20 @@ $.post("http://localhost/creaciones/cajax/buscarcliente",
 			$('#formcliente').removeClass('hide');
 
 			$('#identidad').val(buscar);
+			$('.contenedor_json').addClass('hide');
 
 		}else{
-			console.log(data);
+			//alert(data.nombres);
+			
 
 			 var obj=JSON.parse(data);
 
+			 console.log(obj[0].nombres);
+
 			
 
-			 $.each(obj,function(i,item){
-				$(".contenedor_json").append('<li>' + item.nombres + '</li>');
-
-
+			$.each(obj,function(i,items){
+				$(".contenedor_json").append('<span class="text-danger"><strong> El Cliente ' + items.nombres+'  '+items.apellidos + ' Ya se encuentra registrado..<strong></span>');
 			});
 			
 			//$('#formcliente').addClass('hide');*/
