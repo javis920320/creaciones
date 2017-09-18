1939eae61b28accfc43a2a4764948e52c217ced2
javier <javis9203@gmail.com>
2017-09-18 17:52:56 -0500

    cambios  vista
    configuracion  vista  ingreso pedidos

--- application/views/vpedidomultiple.php
+++ application/views/vpedidomultiple.php
@@ -3,6 +3,88 @@
 	<title></title>
 </head>
 <body>
+<div class="box-body">
 
+			<div class="col-sm-4">
+				<div class="form-group">
+					<label for="factura">Factura:</label>
+
+				 <input type="text" placeholder="N Factura" id="factura" class="form-control">
+
+			</div>
+
+			</div>	
+
+
+			<div class="col-sm-4">
+				<div class="form-group">
+					<label for="facultad">Facultad:</label>
+
+				 <input type="text" placeholder="Ingresar facultad" id="facultad" class="form-control">
+
+				</div>
+
+			</div>	
+
+
+			<div class="col-sm-4">
+				<div class="form-group">
+					<label for="facultad">Identificacion:</label>
+
+				 <input type="text" placeholder="Cedula" id="identificacion" class="form-control">
+
+				</div>
+
+			</div>	
+
+			<div class="col-sm-4">
+				<div class="form-group">
+					<label for="facultad">Tipo Producto:</label>
+
+				<select class="form-control">
+					<option>prueba</option>
+					<option>prueba</option>
+					<option>prueba</option>
+					<option>prueba</option>	
+				</select>
+
+
+				</div>
+
+			</div>	
+			<div class="col-sm-4">
+				<div class="form-group">
+					<label for="cantidad">Cantidad</label>
+
+					<input type="number" id="cantidad" class="form-control">	
+				
+
+				</div>
+
+
+
+			</div>	
+			<div class="col-sm-4">
+				<div class="form-group">
+					<label for="facultad">Tipo Producto:</label>
+
+				<select class="form-control">
+					<option>prueba</option>
+					<option>prueba</option>
+					<option>prueba</option>
+					<option>prueba</option>	
+				</select>
+
+
+				</div>
+
+			</div>	
+
+
+
+
+		
+		
+		</div>
 </body>
 </html>
