182d31b5d07c83344ce4d95d329c54791135904b
javier lopez <javis9203@gmail.com>
2017-10-24 07:04:48 -0500

    j
    h

--- application/models/Madmin.php
+++ application/models/Madmin.php
@@ -17,11 +17,18 @@ class Madmin extends CI_Model
 
  public  function consultafiltropro($param){
 
-	$this->db->select('pr.idproceso,pd.factura,p.nomprod,pd.descripcion,pr.cantidad,pr.precio,pr.precio1');
+	/*$this->db->select('pr.idproceso,pd.factura,p.nomprod,pd.descripcion,pr.cantidad,pr.precio,pr.precio1');
 	$this->db->from('proceso pr');
 	$this->db->join('producto p','p.id_prod=pr.id_prod');
 	$this->db->join('join pedido pd', 'pd.idpedido=pr.idpedido');
-	$this->db->where('pr.idtrabajador',$param['idtrabajador']);
+	$this->db->where('pr.idtrabajador',$param['idtrabajador']);*/
+
+	 $query=$this->db->query("select pr.idproceso,pd.factura,p.nomprod,pd.descripcion,pr.cantidad,pr.precio,pr.precio1 
+	 					from proceso pr inner join producto p on p.id_prod=pr.id_prod inner join pedido pd on pd.idpedido=pr.idpedido
+						where pr.idtrabajador=".$param['idtrabajador']." and pr.fecha  between ".$param['fechaini']." and ".$param['fechafin']."
+	 					");
+
+	return $query->result();
 
 
 
