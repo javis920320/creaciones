4b82eba3711f00c2f16a1bb67d61ded1640f8192
javier lopez <javis9203@gmail.com>
2017-11-22 21:16:58 -0500

    investigar phpExcel

--- application/controllers/Creporte.php
+++ application/controllers/Creporte.php
@@ -1,21 +1,61 @@
 <?php 
 
-class Creporte extends PHPExcel
+class Creporte extends CI_Controller
 {
 	
+
+
+
+ public function __construct()
+    {
+       parent::__construct();
+        //cargamos la libreria html2pdf
+        $this->load->library('Excelfile');
+        //$this->load->library('PHPExcel/PHPExcel_IOFactory');
+        //cargamos el modelo pdf_model
+       // $this->load->model('pdf_model');
+    }
+
+
+
 	public  function index(){
 
 
-	$this->load->library('Excelfile');
+	//$this->load->view('pruebas');
+		$archivo="./plantilla/reporte.xlsx";
+		$plantilla=$this->phpexcel;
+		$plantilla->setActiveSheetIndex(0)->setCellValue('A1','Nombre');
+		$plantilla->setActiveSheetIndex(0)->setCellValue('A2','Apellidos');
 
-	$file="./uploads/Book1.xlsx";
-	$obj=PHPExcel_IOFactory::load($file);
-	$cell=$obj->getActiveSheet()->getCellCollection();
+		$plantilla->getActiveSheet()->setTitle('Report');
+
+		$objgrabar=PHPExcel_IOFactory::createWriter($plantilla,'Excel2007');
+
+		$objgrabar->save($archivo);
+
+
 
 	}
 
 
 ///https://www.youtube.com/watch?v=SAeDi_vNjaQ
+
+
+	public  function tabla(){
+
+
+		//$this->load->library('PHPExcel');
+		$archivo="./plantilla/reporte.xlsx";
+		$plantilla=$this->PHPExcel;
+		$plantilla->setActiveSheetIndex(0)->setCellValue('A1','Nombre');
+		$plantilla->setActiveSheetIndex(0)->setCellValue('A2','Apellidos');
+
+		$plantilla->getActiveSheet()->setTitle('Report');
+
+		$objgrabar=PHPExcel_IOFactory::createWriter($plantilla,'Excel2007');
+
+		$objgrabar->save($archivo);
+	}
 
 
 }
--- application/libraries/Excelfile.php
+++ application/libraries/Excelfile.php
@@ -1,18 +1,15 @@
 <?php 
 
-require_once APPPATH."/third_party/PHPExcel.php";
-class Excelfile extends PHPExcel
-{
-	
-	function __construct()
-	{
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-	parent::__construct();
-	}
+ // require_once APPPATH."./third_party/PHPExcel/PHPExcel.php";
+  //require_once APPPATH."./third_party/PHPExcel/IOFactory.php";
 
+ class Excelfile extends PHPExcel {
+       public function __construct() {
+       parent::__construct();
+   }
+ }
+https://stackoverflow.com/questions/15028175/phpexcel-error-in-codeigniter-unable-to-load-the-requested-class-iofactory
 
-///https://www.youtube.com/watch?v=SAeDi_vNjaQ
-
-
-}
  ?>
