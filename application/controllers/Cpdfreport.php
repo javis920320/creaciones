5b70da44865029b96497dd3f3af20436a8172083
javier <javis9203@gmail.com>
2017-11-29 11:38:08 -0500

    nuevos  cambios para prueba  pdf
    pdf

---
+++ application/controllers/Cpdfreport.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Cpdfreport extends CI_Controller {
+
+  /**
+    * Get All Data from this method.
+    *
+    * @return Response
+   */
+   public function index()
+   {
+	$this->load->view('mypdf');
+   }
+
+   /**
+    * Get Download PDF File
+    *
+    * @return Response
+   */
+   function mypdf(){
+
+	$this->load->library('pdf');
+
+  	$this->pdf->load_view('mypdf');
+  	$this->pdf->render();
+
+  	$this->pdf->stream("Cpdfreport.pdf");
+   }
+}
