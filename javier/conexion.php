2fcfc42982188c859fa31583b3f7d2809f2ed529
javier <javis9203@gmail.com>
2018-01-11 15:04:29 -0500

    limpiando
    limpiandoi

--- javier/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
- 
-fscanf($handle,"%d",$n);
-$arr_temp = fgets($handle);
-$arr = explode(" ",$arr_temp);
-array_walk($arr,'intval');
-
- ?>
--- javis.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set mydate=%date:~6,4%%date:~3,2%%date:~0,2%
-
-c:\xampp\mysql\bin\mysqldump.exe -u root -p --password='' creaciones > c:\xampp\htdocs\%mydate%.sql
