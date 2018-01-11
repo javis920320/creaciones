00c2426e2fb62954fd41d83b6dc647686457f6d3
javier <javis9203@gmail.com>
2018-01-11 15:16:20 -0500

    hoy cambios
    hoy cambios

---
+++ application/views/vsatelite.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	
+</head>
+<body>
+	<style type="text/css">
+	.responsive-input{
+  width: 100%;
+}
+	</style>
+
+<div class="col-xs-17"> <strong align='center'>Creaciones Goretti</strong></div>
+
+
+
+  
+  
+ <!-- Modal -->
+ <div class="modal fade" id="asignar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+   <div class="modal-dialog" role="document">
+     <div class="modal-content">
+       <div class="modal-header">
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+         <h4 class="modal-title" id="myModalLabel">CREACIONES GORETTI</h4>
+       </div>
+       <div class="modal-body">
+         <div class='form-group'>
+          <input type='number' id='fac' name='fac' class='form-control'>
+          <button class='btn btn-primary 'onclick='buscar();'>Buscar pedido</button>
+         </div>
+
+         <div id="res"></div>
+         <div id="tpprod"></div>
+       </div>
+       <div class="modal-footer">
+         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+         
+       </div>
+     </div>
+   </div>
+ </div>
+
+
+<div class="col-xs-12 col-md-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title" class='text-primary'> SATELITES CREACIONES </h3><br><br>
+              
+<button id='pdf' class='btn '  data-toggle="modal" data-target="#asignar"><span class='glyphicon glyphicon-sum'></span>  Asignar proceso</button>
+
+
+
+            </div>
+            <!-- /.box-header -->
+             
+            <div class="box-body table-responsive no-padding">
+              <table  id='tblproductosen' class="table table-hover table-responsive">
+                <thead class="bg-success">
+                <tr>
+                  <th>Factura</th>
+                  <th>Producto</th>
+                  <th>Facultad</th>
+                   <th>Cantidad</th>
+                   <th>Talla</th>
+                   <th>Descripcion</th>
+                   <th>Cliente</th>
+                  <th>Fecha ingreso</th>
+                  <th>Estados</th>
+                  <th>Accion</th>
+                </tr>
+                </thead>
+                <tbody></tbody>
+                
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <script type="text/javascript">
+	var baseurl = "<?php echo base_url(); ?>";
+</script>
+	
+</body>
+</html>
