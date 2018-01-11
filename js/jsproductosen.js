be67843f3f283e977133aad3ed4f41ad9b3340a5
javier <javis9203@gmail.com>
2018-01-11 14:24:40 -0500

    Revert "ultimos  cambios  en envios"
    This reverts commit 8e49017f0ac1ac5e23e76182368ed94b5f5b83b5.

--- js/jsproductosen.js
+++ js/jsproductosen.js
@@ -3,7 +3,6 @@ $('#tblproductosen').DataTable({
 			'info':true,
 			'filter':true,
 			'stateSave':true,
-			'destroy':true,
 
 			'ajax':{
 
@@ -29,21 +28,20 @@ $('#tblproductosen').DataTable({
 
 
 				return '<span class="pull-right">' +
-					  '<input type="checkbox" class="chk" name="chk"  value='+row.idpedido+'>'+	
-					  '<a  href="#"  class="btn btn-default  btn-sm" style="width:80%;" title="Enviar informacion" data-toggle="modal" data-target="#enviar" onClick="selpedido(\''+row.idpedido+'\');"><i class="glyphicon glyphicon-wrench"></i><span> En proceso</span></a>'+
-                     /* '<div class="dropdown">' +
+					  '<input type="checkbox" class="chk" name="chk" value='+row.idpedido+'>'+	
+                      '<div class="dropdown">' +
                       '  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">' +
                       '    Acciones' +
                       '  <span class="caret"></span>' +
                       '  </button>' +
                       '    <ul class="dropdown-menu pull-right" aria-labelledby="dropdownMenu1">' +
-                        '    <li><a  href="#"  class="btn btn-default  btn-sm" style="width:80%;" title="Enviar informacion" data-toggle="modal" data-target="#enviar" onClick="selpedido(\''+row.idpedido+'\');"><i class="glyphicon glyphicon-wrench"></i><span> En proceso</span></a></li>' +
+                        '    <li><a  href="#"  class="btn btn-success  btn-sm" style="width:80%;" title="Enviar informacion" data-toggle="modal" data-target="#enviar" onClick="selpedido(\''+row.idpedido+'\');"><i class="glyphicon glyphicon-wrench"></i><span> En proceso</span></a></li>' +
                       //'    <li><a href="'+baseurl+'cafiliado/descargar/'+row.idPersona+'" title="Imprimir formato"><i class="glyphicon glyphicon-print" style="color:#006699"></i> Imprimir</a></li>' +
                      // '    <li><a href="#" title="Ajustar precios"  data-toggle="modal" data-target="#precios"  onClick="ajustarprecios(\''+row.id_prod+'\',\''+row.valor+'\',\''+row.subvalor+'\')"><i style="color:green;" class="glyphicon glyphicon-usd"></i> Ajustar valores</a></li>' +
                        //'    <li><a href="#" title="Asignar bordados" class="prueba" data-toggle="modal" data='+row.id_prod+' data-target="#asgbordados"  onClick="bordprod(\''+row.id_prod+'\')"><i style="color:green;" class="glyphicon glyphicon-paperclip"></i> Asignar bordados</a></li>' +
                       //'    <li><a href="#" title="Desaprobar afiliado" onClick="updEstadoAfiliado('+row.idPersona+','+2+')"><i style="color:red;" class="glyphicon glyphicon-remove"></i> Desaprobar</a></li>' +
                       '    </ul>' +
-                      '</div>' +*/
+                      '</div>' +
                       '</span>';
 
 					
@@ -138,22 +136,11 @@ $.ajax({
           data: {'array':JSON.stringify(selecciones)},//capturo array     
           success: function(objView){
 			  alert(objView);
-			  $('#tblproductosen').DataTable().ajax.reload();
 
         }
 });
   
 	 }
-	 
-	 
-	 
-	 $('input[name="mtodo"]').on('change',function(){
-		if ($('#mtodo').prop('checked') ) {
-    $('.chk').prop('checked',true);
-		}else{
-			 $('.chk').prop('checked',false);
-		}
-	 });
 /*
 selpedido = function(idpedido){
 	$('#producto').val(idpedido);
