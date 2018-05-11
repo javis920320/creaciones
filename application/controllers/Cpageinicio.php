85624aa29e32839bd2671e36edc3b79577f92b98
javis920320 <javis9203@gmail.com>
2018-05-10 20:20:55 -0500

    uuu
    kkk

---
+++ application/controllers/Cpageinicio.php
@@ -0,0 +1,30 @@
+<?php 
+
+/**
+* 
+*/
+class Cpageinicio extends CI_Controller
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+
+
+	 public  function index(){
+
+	 	$this->load->view('vfanpage');
+
+	 }
+
+
+
+
+}
+
+
+
+
+ ?>
---
+++ application/views/vfanpage.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+
+	<title>Creaciones</title>
+</head>
+<body>
+<div class="container">
+	<br>
+
+
+
+	<div class="col-md-12">
+		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+
+			<ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+
+
+			<div class="carousel-inner" role="listbox">
+				<div class="item active">
+      <img src="https://admin.kienyke.com/wp-content/uploads/2017/10/tecnologia.png" alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item">
+      <img src="..." alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+				
+			</div>
+			
+
+
+		</div>
+
+	</div>
+</div>
+
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
